6f6121cb390703f8ec5fc6c3d70fcaa51cdbabe5
bvreeman <bvreeman@gmail.com>
2018-09-04 14:28:36 -0500

    working on gallery and started setting up firebase storage. checking that the env file is not going up to github

--- src/pages/Gallery/Gallery.js
+++ src/pages/Gallery/Gallery.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import './Gallery.css';
 
-
-// Components
-// import About from '../../components/About';
-// import Carousel from '../../components/Carousel';
-// import Draggable from '../../components/Draggable';
-// import withAuthorization from '../../components/FirebaseWithAuthorization'
 class Gallery extends React.PureComponent {
     render() {
         return (
             <div>
                 <div>
-                    {/* <Carousel /> */}
                 </div>
                 <div className="container">
                     {/* <About /> */}
