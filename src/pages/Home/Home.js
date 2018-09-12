578761e6f8e88de1a667be86abf0e49529479c8a
bvreeman <bvreeman@gmail.com>
2018-09-11 22:38:35 -0500

    added some icons to the home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Home.css';
 import nightTruck from './bigger-night-truck.jpg'
+import truckIconRight from './truckFacingRight.png'
 
 // Components
 // import About from '../../components/About';
@@ -13,6 +14,20 @@ class Home extends React.PureComponent {
             <div className="homePage">
                 <div className="container-fluid mainPhotoDiv">
                     <img className='mainPhoto' src={nightTruck} alt="One of our trucks at night"/>
+                </div>
+                <div className="row">
+                    <div className="col-md-4 col-xs-12">
+                        <img className='truckIcon' src={truckIconRight} alt="Truck Icon"/>
+                        <h4 className='truckIconHeading'>Truck and Trailer Repair</h4>
+                    </div>
+                    <div className="col-md-4 col-xs-12">
+                        <img className='truckIcon' src={truckIconRight} alt="Truck Icon"/>
+                        <h4 className='truckIconHeading'>Refrigerated</h4>
+                    </div>
+                    <div className="col-md-4 col-xs-12">
+                        <img className='truckIcon' src={truckIconRight} alt="Truck Icon"/>
+                        <h4 className='truckIconHeading'>Truck and Equipment Wash</h4>
+                    </div>
                 </div>
             </div>
         )
