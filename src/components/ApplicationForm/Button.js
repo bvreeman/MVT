f9958637af626a4bdc482eed5454098bcf26f267
bvreeman <bvreeman@gmail.com>
2018-10-03 16:53:02 -0500

    added a button feature to hopefully email

---
+++ src/components/ApplicationForm/Button.js
@@ -0,0 +1,39 @@
+import React, { PureComponent, PropTypes } from 'react';
+
+
+class Button extends PureComponent {
+  static propTypes = {
+    email: PropTypes.string.isRequired,
+    formValues: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      email: PropTypes.string.isRequired,
+      message: PropTypes.string.isRequired,
+    }).isRequired,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isClicked: false,
+    };
+    this.logFormDataToConsole = this.logFormDataToConsole.bind(this);
+  }
+
+  logFormDataToConsole(event) {
+    console.log('Form Values', this.props.formValues);
+    this.setState({ isClicked: true });
+  }
+
+  render() {
+    return (
+      <button
+        disabled={this.state.isClicked}
+        onClick={this.logFormDataToConsole}
+      >
+        Send Application
+      </button>
+    );
+  }
+}
+
+export default Button;
