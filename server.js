c7fb04e34d68ec420daab8d8f7f5041318b6189d
bvreeman <bvreeman@gmail.com>
2018-10-10 10:02:05 -0500

    small changes in server to work through heroku issues

--- server.js
+++ server.js
@@ -21,11 +21,11 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
 }
 // Add routes, both API and view
-app.use(routes)
+app.use('/', routes)
 
-app.post('/', (req, res) => {
-  res.send();
-})
+// app.post('/', (req, res) => {
+//   res.send();
+// })
 
 app.post('/api/form', (req, res) => {
   console.log(req.body)
