ef7ac31304d04585b04bd340368fcb22d92e9c89
bvreeman <bvreeman@gmail.com>
2018-10-10 10:20:34 -0500

    changed the app.get from public to build

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-// const mongoose = require("mongoose");
 const nodemailer = require('nodemailer')
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
-// const methodOverride = require('method-override');
 const path = require('path');
 
 // Define middleware here
@@ -27,23 +25,14 @@ app.use('/', routes)
 //   res.send();
 // })
 
-app.post('/api/form', (req, res) => {
-  console.log(req.body)
-  res.send();
-})
-
-// mongoose.Promise = global.Promise;
-
-// // Connect to the Mongo DB
-// const dbUri = process.env.MONGODB_URI || "mongodb://localhost:27017/MVT";
-// // const dbUri = process.env.MONGODB_URI || "mongodb://rikkihon:melisandrebran99$@ds231501.mlab.com:31501/users";
-
-
-// mongoose.connect(dbUri).then(() => console.log('connected to DB!')).catch((err) => console.log(err));
+// app.post('/api/form', (req, res) => {
+//   console.log(req.body)
+//   res.send();
+// })
  
 // If no API routes are hit, send the React app
 app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, "/client/public/index.html"));
+  res.sendFile(path.join(__dirname, "/client/build/index.html"));
 });
 
 // Start the API server
