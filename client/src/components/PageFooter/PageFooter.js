0d5b160642625f8fe7d93696213319a19c15b0ba
bvreeman <bvreeman@gmail.com>
2018-10-19 16:54:15 -0500

    changed footer layout a media query for the facebook logo link

--- client/src/components/PageFooter/PageFooter.js
+++ client/src/components/PageFooter/PageFooter.js
@@ -23,32 +23,30 @@ class PageFooter extends React.PureComponent {
                     </Container>
                 </div>
                 <Container className="mt-5 mb-4 text-center text-md-left">
-                    <Row className="mt-3">
-                        <Col md="3" lg="4" xl="3" className="mb-4">
+                    <Row className="mt-3 footerText">
+                        <Col md="4" lg="4" xl="4" className="mb-4">
                             <h6 className="text-uppercase font-weight-bold"><strong>Minnesota Valley Transport</strong></h6>
-                            <hr className="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style={{width: '60px'}}/>
+                            <hr className="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" />
                             <p>Here you can use rows and columns here to organize your footer content. Lorem ipsum dolor sit
                                 amet, consectetur adipisicing elit.</p>
                         </Col>
-                        <Col md="2" lg="2" xl="2" className="mb-4">
-                            <h6 className="text-uppercase font-weight-bold"><strong>Products</strong></h6>
-                            <hr className="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style={{width: '60px'}}/>
-                            <p><a href="#!">MDBootstrap</a></p>
-                            <p><a href="#!">MDWordPress</a></p>
-                            <p><a href="#!">BrandFlow</a></p>
-                            <p><a href="#!">Bootstrap Angular</a></p>
+                        <Col md="4" lg="4" xl="4" className="mb-4">
+                            <h6 className="text-uppercase font-weight-bold"><strong>Affiliations</strong></h6>
+                            <hr className="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" />
+                            <p><a href="https://www.mntruck.org/" target="_blank" rel="noopener noreferrer">Minnesota Trucking Association</a></p>
+                            <p><a href="https://www.epa.gov/smartway" target="_blank" rel="noopener noreferrer">SmartWay</a></p>
                         </Col>
-                        <Col md="3" lg="2" xl="2" className="mb-4">
+                        {/* <Col md="3" lg="2" xl="2" className="mb-4">
                             <h6 className="text-uppercase font-weight-bold"><strong>Useful links</strong></h6>
-                            <hr className="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style={{width: '60px'}}/>
+                            <hr className="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" />
                             <p><a href="#!">Your Account</a></p>
                             <p><a href="#!">Become an Affiliate</a></p>
                             <p><a href="#!">Shipping Rates</a></p>
                             <p><a href="#!">Help</a></p>
-                        </Col>
-                        <Col md="4" lg="3" xl="3" className="mb-4">
+                        </Col> */}
+                        <Col md="4" lg="4" xl="4" className="mb-4">
                             <h6 className="text-uppercase font-weight-bold"><strong>Contact</strong></h6>
-                            <hr className="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style={{width: '60px'}}/>
+                            <hr className="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" />
                             <p><i className="fa fa-home mr-3"></i> New Ulm, MN 56073, US</p>
                             <p><i className="fa fa-envelope mr-3"></i> dan@mnvalleytransport.com</p>
                             <p><i className="fa fa-phone mr-3"></i> (507) 354-3276</p>
